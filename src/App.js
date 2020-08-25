601c43ac7952ff4d48ae9454435304bdcacfda37
K1nGsmaN-hub <15162342h@gmail.com>
2020-08-26 02:47:46 +0800

    Added "generators" functions, functions "working with sequences", "active state" functions, "changed rounds" functions, "game processing" functions and other functions Import sounds. Added new state props: delay, difficulties

--- src/App.js
+++ src/App.js
@@ -3,78 +3,211 @@ import React, {Component} from 'react';
 import GameArea from './components/game-area/game-area'
 import OptionsArea from "./components/options-area/options-area";
 
+import soundBlue from './assets/sounds/1.mp3'
+import soundRed from './assets/sounds/2.mp3'
+import soundYellow from './assets/sounds/3.mp3'
+import soundGreen from './assets/sounds/4.mp3'
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       round: 0,
+      delay: 1500,
       gameSequence: [],
       userSequence: [],
+      isGaming: false,
       buttons: [
-        {color: 'blue', key: this.randomKey(), id: 1, active: false},
-        {color: 'red', key: this.randomKey(), id: 2, active: false},
-        {color: 'yellow', key: this.randomKey(), id: 3, active: false},
-        {color: 'green', key: this.randomKey(), id: 4, active: false},
-      ]
+        {color: 'blue', sound: new Audio(soundBlue), key: this.randomKey(), id: 1, active: false},
+        {color: 'red', sound: new Audio(soundRed), key: this.randomKey(), id: 2, active: false},
+        {color: 'yellow', sound: new Audio(soundYellow), key: this.randomKey(), id: 3, active: false},
+        {color: 'green', sound: new Audio(soundGreen), key: this.randomKey(), id: 4, active: false},
+      ],
+      difficulties: [
+        {level: 'easy', delay: 1500, text: 'Легкий', key: this.randomKey()},
+        {level: 'medium', delay: 1000, text: 'Средний', key: this.randomKey()},
+        {level: 'hard', delay: 400, text: 'Сложный', key: this.randomKey()}
+      ],
     }
   }
 
+  // generators
   randomKey = () => {
     return Math.random().toString(36).substr(6)
-  }
-
+  } // return string in 6 chars
   randomNum = () => {
     return Math.round(0.5 + Math.random() * 4)
-  }
-
+  } // return number in 1 through 4
   generateStep = () => {
     this.setState(({gameSequence}) => {
-      const newArr = gameSequence
-      newArr.push(this.randomNum)
-
+      const newArr = [...gameSequence]
+      newArr.push(this.randomNum())
       return {
         gameSequence: newArr
       }
     })
-  }
+  } // push new elem to gameSequence
 
-  onActive = (key) => {
-    const updateState = (boolean) => {
-      this.setState(({buttons}) => {
-        const newArr = buttons
-        newArr.forEach(item => {
-          if (item.key === key) {
-            item.active = !!boolean
-          }
-        })
-        return {
-          buttons: newArr
+
+  // work with sequences
+  clearSequence = (start = false) => {
+    if (start) {
+      this.setState({
+        userSequence: [],
+        gameSequence: []
+      })
+    } else {
+      this.setState({userSequence: []})
+    }
+  } // clear the sequences
+  fillUserSequence = (key) => {
+    this.setState(({userSequence}) => {
+      const {buttons} = this.state
+      const newArr = [...userSequence]
+      newArr.push(buttons.findIndex(item => item.key === key) + 1)
+
+      return {
+        userSequence: newArr
+      }
+    })
+  } // add new num to userSequence
+  compareSequences = () => {
+    const {gameSequence, userSequence} = this.state
+
+    if (userSequence.length > gameSequence.length) {
+      return false
+    } else {
+      let equals = 0
+      userSequence.forEach((item, i) => {
+        if (item === gameSequence[i]) {
+          equals++
         }
       })
+      if (equals === userSequence.length) {
+        return true
+      }
     }
-    const req = new Promise((resolve, reject) => {
-      updateState(1)
+  } // compares userSequence and gameSequence
+  demoSequence = () => {
+    const {gameSequence, buttons, delay} = this.state
 
+    gameSequence.forEach((step, i) => { // iterating gameSequence array
       setTimeout(() => {
-        resolve(key)
-      }, 1000)
 
+        buttons.forEach(btn => { // iterating buttons array
+          if (btn.id === step) {
+            this.activeState(btn.key)
+          }
+        })
+
+      }, delay * i)
     })
-    .finally(() => {
-        updateState(0)
+  } // show gameSequence
+
+
+  // active state
+  onActive = (key) => {
+    this.activeState(key)
+        .then(() => {
+          const {isGaming} = this.state
+          if (isGaming) {
+            this.fillUserSequence(key)
+            this.gameSession()
+                .catch(() => console.log('Произошла ошибка'))
+          }
+        })
+  } // active state for buttons and update userSequence
+  activeState = async (key) => {
+    this.updateActive(key)
+    this.playSound(key)
+    await setTimeout(() => {this.updateActive(key)}, 200)
+  } // active state for demonstrations
+  updateActive = (key) => {
+    this.setState(({buttons}) => {
+      const newArr = [...buttons]
+
+      newArr.forEach(item => {
+        if (item.key === key) {
+          item.active = !item.active
+        }
+      })
+
+      return {
+        buttons: newArr
+      }
     })
+  } // update active button's state
 
-    console.log(req)
-  }
 
+  // rounds
+  newRound = async (start = false) => {
+    this.setState(({round}) => {
+      const roundNum = (start) ? 1 : ++round
+      return {
+        round: roundNum
+      }
+    })
+    await this.clearSequence(start)
+    await this.generateStep()
+  } // change round number and call generateStep() and demoSequence
+  startNewRound = () => {
+    this.newRound()
+        .then(() => console.log('Start Round ' + this.state.round))
+        .then(this.demoSequence)
+
+  } // new round
+
+
+  // game process
   startGame = () => {
-    this
-  }
+    this.setState({isGaming: true})
+    this.newRound(true)
+        .then(() => console.log('Start Round 1'))
+        .then(this.demoSequence)
+
+    console.log('Game is start')
+  } // start gameSession
+  gameSession = async () => {
+    const {gameSequence, userSequence} = this.state
+    const compareResult = await this.compareSequences()
+
+    if (!compareResult) {
+      this.gameOver()
+    } else if (userSequence.length === gameSequence.length && compareResult) {
+      setTimeout(this.startNewRound, 1000)
+    }
+  } // data processing of sequences
+  gameOver = () => {
+    this.clearSequence(true)
+    this.setState({round: 0, isGaming: false})
+
+    console.log('Game Over :(')
+  } // end game session
+
+
+  // other
+  setDelay = (delay) => {
+    this.setState({delay})
+  } // delay between activating buttons
+  playSound = (key) => {
+    const {buttons} = this.state
+    buttons.forEach(item => {
+      if (item.key === key) {
+        item.sound.play()
+      }
+    })
+  } // click or demo to play sound
+  show = () => {
+    const {userSequence, gameSequence} = this.state
+    console.log(`user: ${userSequence}`)
+    console.log(`game: ${gameSequence}`)
+    console.log(`delay: ${this.state.delay}`)
+  } // dev func
 
   render() {
     return (
       <div className="app">
-        <h1 className="title">Simon Says</h1>
+        <h1 className="title" onClick={this.show}>Simon Says</h1>
         <GameArea
           buttons={this.state.buttons}
           onActive={this.onActive}
@@ -82,6 +215,8 @@ class App extends Component {
         <OptionsArea
             round={this.state.round}
             startGame={this.startGame}
+            diffs={this.state.difficulties}
+            setDelay={this.setDelay}
         />
       </div>
     )
