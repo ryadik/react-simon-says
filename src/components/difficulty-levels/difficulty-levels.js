c82ff2ea6f9650a03242769a04c6eb160fd6f99a
K1nGsmaN-hub <15162342h@gmail.com>
2020-08-26 02:40:08 +0800

    added difficulty-levels component

---
+++ src/components/difficulty-levels/difficulty-levels.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+
+export default class DifficultyLevels extends Component {
+  render() {
+    const {level, delay, text, setDelay} = this.props
+    return (
+      <>
+        <input type="radio"
+               name="difficulty"
+               className="difficulty-levels"
+               data-level={level}
+               data-delay={delay}
+               defaultChecked={delay === 1500}
+               onClick={setDelay}
+        />
+        {text}
+      </>
+    )
+  }
+}
