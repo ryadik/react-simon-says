75f4b801613477026828f49b8f5e913d5bb62d42
K1nGsmaN-hub <15162342h@gmail.com>
2020-08-26 02:43:11 +0800

    added DifficultyLevels component to options-area

--- src/components/options-area/options-area.js
+++ src/components/options-area/options-area.js
@@ -2,22 +2,43 @@ import React, {Component} from "react";
 
 import OptionsRound from "../options-rounds/options-round";
 import StartButton from "../start-button/start-button";
+import DifficultyLevels from "../difficulty-levels/difficulty-levels";
 
 import './options-area.sass'
 
+
 export default class OptionsArea extends Component {
+  generateItem = (arr) => {
+    const {setDelay} = this.props
+    return arr.map(item => {
+      return (
+          <label key={item.key}>
+            <DifficultyLevels
+                level={item.level}
+                delay={item.delay}
+                text={item.text}
+                setDelay={() => setDelay(item.delay)}
+            />
+          </label>
+      )
+    })
+  }
+
   render() {
-    const {startGame} = this.props
+    const {startGame, diffs} = this.props
 
     let {round} = this.props
     if (round === undefined || round === 0 || isNaN(round)) {
       round = 0
     }
 
+    const elems = this.generateItem(diffs)
+
     return(
         <div className="options-area">
           <OptionsRound round={round}/>
           <StartButton startGame={() => startGame()}/>
+          {elems}
         </div>
     )
   }
