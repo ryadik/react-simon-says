bfa53b44117890cef1f2827d306ee03e3b635655
K1nGsmaN-hub <15162342h@gmail.com>
2020-08-26 19:02:49 +0800

    Updates

---
+++ precache-manifest.0c6185b84031f9d5b5d2d4f9bdeee6d7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9954ec0c7b4124bbb2f2181d15b750c7",
+    "url": "/react-simon-says/index.html"
+  },
+  {
+    "revision": "1902650ce6d5bf3c26f7",
+    "url": "/react-simon-says/static/css/main.0dda3b68.chunk.css"
+  },
+  {
+    "revision": "1212acb41ac16737ecba",
+    "url": "/react-simon-says/static/js/2.eebb11ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-simon-says/static/js/2.eebb11ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1902650ce6d5bf3c26f7",
+    "url": "/react-simon-says/static/js/main.3fb0e51d.chunk.js"
+  },
+  {
+    "revision": "7f8af093c2a9d47270b3",
+    "url": "/react-simon-says/static/js/runtime-main.d94022b1.js"
+  },
+  {
+    "revision": "99e50f7d6744bfe49381ee010a0ed8ad",
+    "url": "/react-simon-says/static/media/1.99e50f7d.mp3"
+  },
+  {
+    "revision": "d2c4a2c42f3ff79a6674c18fa42aba97",
+    "url": "/react-simon-says/static/media/2.d2c4a2c4.mp3"
+  },
+  {
+    "revision": "c1c96b4438f582241dcdee51f5189b79",
+    "url": "/react-simon-says/static/media/3.c1c96b44.mp3"
+  },
+  {
+    "revision": "36d5a7222077f946d6dbc028b5b8a8a6",
+    "url": "/react-simon-says/static/media/4.36d5a722.mp3"
+  }
+]);
